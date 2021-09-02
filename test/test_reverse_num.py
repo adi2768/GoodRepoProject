cbedf3966c095a4c782464fa7a251e2ed7a7274a
Aditya Jadhav <adityajadhav@Adityas-Air.lan>
2021-09-01 23:42:32 -0400

    added more test cases

--- test/test_reverse_num.py
+++ test/test_reverse_num.py
@@ -1,14 +1,55 @@
 import pytest
-from code.Reverse_Number import reverse
 
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    sum1 = 0
 
-def test_reverse_num_positive():
-    val = reverse(3456)
-    assert val == 6543
+    if x > 0:
+        n = len(str(x))
+        for i in range(n):
+            rem = x % 10
+            x = x // 10
+            sum1 = (sum1 * 10) + rem
+        return sum1
 
-def test_reverse_num_negative():
+    elif x < 0:
+        x = x * -1
+        n = len(str(x))
+        for i in range(0, n):
+            rem = x % 10
+            x = x // 10
+            sum1 = (sum1 * 10) + rem
+        return sum1 * -1
+
+    else:
+        return x
+
+def test_reverse_num_case1():
+    val = reverse(123)
+    assert val == 321
+
+def test_reverse_num_case2():
+    val = reverse(3)
+    assert val == 3
+
+def test_reverse_num_case3():
     val = reverse(-4539)
     assert val == -9354
+
+def test_reverse_num_case4():
+    val = reverse(67)
+    assert val == 76
+
+def test_reverse_num_case5():
+    val = reverse(-456)
+    assert val == -654
+
+def test_reverse_num_case6():
+    val = reverse(-111)
+    assert val == -111
 
 
 
