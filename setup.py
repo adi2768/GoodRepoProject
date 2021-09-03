257c2b0e9789b615458d56e12fcb7cda736a2986
Harsh Vijay Bathija <37671963+HarshBathija@users.noreply.github.com>
2021-09-02 20:41:44 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='GoodRepoProject',
+      version='1.0',
+      description='Software Engineering Homework Assignment 2b',
+      author='Aditya, Harsh, Ashritha, Abhishek, Akash',
+      author_email='adityajadhavncsu@gmail.com',
+      url='https://github.com/adi2768/GoodRepoProject.git',
+      packages=['GoodRepoProject'],
+      license='MIT'
+     )
